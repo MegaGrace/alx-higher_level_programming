656184834276443143473731337af707cfc241e2
MegaGrace <danmarley2000@yahoo.com>
2021-11-17 22:29:55 +0000

    last digit update

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number >= 0:
-        last_digit = number % 10
+if number > 0:
+            last = number % 10
+elif number == 0:
+            last = "0"
 else:
-        last_digit = -1 * (abs(number) % 10)
-
-        if last_digit > 5:
-                x = "and is greater than 5"
-        elif last_digit == 0:
-                x = "and is 0"
-        elif last_digit < 6:
-                x = "and is less than 6 and not 0"
-
-                print("Last digit of", number, "is", last_digit, x)
+            last = (number % -10) * -1
+if last > 5:
+        x = "and is greater than 5"
+elif last == 0:
+        x = "and is 0"
+elif last < 6 and last != 0:
+        x = "and is less than 6 and not 0"
+        print("Last digit of {:d} is {:d} {:s}".format(number, last, x))
