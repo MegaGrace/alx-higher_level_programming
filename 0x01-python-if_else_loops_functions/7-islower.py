5d587f2398f76a874f00a775e0a3153e5470566b
MegaGrace <danmarley2000@yahoo.com>
2021-11-19 14:53:28 +0000

    isLower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
-        x = ord(c)
-            if x > 96 and x < 123:
-                        return True
-            else:
-                        return False
+    x = ord(c)
+    if x > 96 and x < 123:
+        return True
+    else:
+        return False
