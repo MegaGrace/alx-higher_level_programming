ba335188e4307d647b07f744d13780b6e513abd7
MegaGrace <danmarley2000@yahoo.com>
2021-11-20 06:55:42 +0000

    uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,7 @@
-ef uppercase(str):
+def uppercase(str):
     for i in range(0, len(str)):
-                case = ord(str[i])
-                        if case > 96 and case < 123:
-                                        case = case - 32
-                                                print("{:c}"
-                                                      .format(case), end="")
-                                                    print("")
+        case = ord(str[i])
+        if case > 96 and case < 123:
+            case = case - 32
+        print("{:c}".format(case), end="")
+    print("")
