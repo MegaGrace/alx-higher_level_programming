e0679d180564c8250a942f485c357f81f42eacc4
MegaGrace <danmarley2000@yahoo.com>
2021-11-21 19:10:16 +0000

    indexes

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx < 0:
+    if idx < 0 or idx > len(my_list) - 1:
         return None
-    elif idx > len(my_list) - 1:
-        return None
-    else:
-        for idx in range(0, 4):
-            result = my_list[idx]
-        return result
+    return my_list[idx]
